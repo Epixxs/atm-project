549e82f73b81550e9941411701e55f830c48056f
Saksham Sharma <102599569+Epixxs@users.noreply.github.com>
2024-12-28 21:18:29 +0530

    Update create-database.py
    changed name on creation

--- ATM Project/atm/create-database.py
+++ ATM Project/atm/create-database.py
@@ -1,4 +1,4 @@
 import mysql.connector as sql
 conn=sql.connect(host='localhost',user='root',password='',)
 cursor = conn.cursor()
-cursor.execute("create database ATM_MACHINE")
+cursor.execute("create database BANKING")
